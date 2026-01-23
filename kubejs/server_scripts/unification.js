f3b2048991ed9c66f2f06014ec959be66acba9c5
cobalt <102835906+cobalt-co27@users.noreply.github.com>
2026-01-23 17:40:11 -0500

    Remove kjs dir to upload new one

--- kubejs/server_scripts/unification.js
+++
@@ -1,7 +0,0 @@
-ServerEvents.recipes(event => {
-    event.replaceInput(
-        {input: "minecraft:diamond", type: "minecraft:crafting"},
-        "minecraft:diamond",
-        "#c:diamonds"
-    )
-})
